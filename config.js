4d4fc7c50349050693efa30ebfe9fa5f3d8f30a9
faultypointer <121874711+faultypointer@users.noreply.github.com>
2023-02-10 02:50:28 +0000

    modifief config to my liking

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'faultypointer',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
@@ -55,19 +55,13 @@ const CONFIG = {
 			id: '1',
 			name: 'Github',
 			icon: 'github',
-			link: 'https://github.com/',
+			link: 'https://github.com/faultypointer',
 		},
 		{
 			id: '2',
 			name: 'Mail',
 			icon: 'mail',
 			link: 'https://mail.protonmail.com/',
-		},
-		{
-			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
 		},
 		{
 			id: '4',
@@ -83,49 +77,49 @@ const CONFIG = {
 		},
 		{
 			id: '6',
-			name: 'Odysee',
+			name: 'youtube',
 			icon: 'youtube',
-			link: 'https://odysee.com/',
+			link: 'https://www.youtube.com/',
 		},
 	],
 
 	secondButtonsContainer: [
-		{
-			id: '1',
-			name: 'Music',
-			icon: 'headphones',
-			link: 'https://open.spotify.com',
-		},
-		{
-			id: '2',
-			name: 'twitter',
-			icon: 'twitter',
-			link: 'https://twitter.com/',
-		},
+		// {
+		// 	id: '1',
+		// 	name: 'Music',
+		// 	icon: 'headphones',
+		// 	link: 'https://open.spotify.com',
+		// },
+		// {
+		// 	id: '2',
+		// 	name: 'twitter',
+		// 	icon: 'twitter',
+		// 	link: 'https://twitter.com/',
+		// },
 		{
 			id: '3',
 			name: 'bot',
 			icon: 'bot',
 			link: 'https://discord.com/app',
 		},
-		{
-			id: '4',
-			name: 'Amazon',
-			icon: 'shopping-bag',
-			link: 'https://amazon.com/',
-		},
+		// {
+		// 	id: '4',
+		// 	name: 'Amazon',
+		// 	icon: 'shopping-bag',
+		// 	link: 'https://amazon.com/',
+		// },
 		{
 			id: '5',
 			name: 'Hashnode',
 			icon: 'pen-tool',
 			link: 'https://hashnode.com/',
 		},
-		{
-			id: '6',
-			name: 'Figma',
-			icon: 'figma',
-			link: 'https://figma.com/',
-		},
+		// {
+		// 	id: '6',
+		// 	name: 'Figma',
+		// 	icon: 'figma',
+		// 	link: 'https://figma.com/',
+		// },
 	],
 
 	// ┬  ┬┌─┐┌┬┐┌─┐
@@ -156,62 +150,62 @@ const CONFIG = {
 				},
 			],
 		},
-		{
-			icon: 'coffee',
-			id: '2',
-			links: [
-				{
-					name: 'Linkedin',
-					link: 'https://www.linkedin.com',
-				},
-				{
-					name: 'Dribbble',
-					link: 'https://www.dribbble.com',
-				},
-				{
-					name: 'Trello',
-					link: 'https://www.trello.com',
-				},
-				{
-					name: 'Slack',
-					link: 'https://www.slack.com',
-				},
-			],
-		},
+		// {
+		// 	icon: 'coffee',
+		// 	id: '2',
+		// 	links: [
+		// 		{
+		// 			name: 'Linkedin',
+		// 			link: 'https://www.linkedin.com',
+		// 		},
+		// 		{
+		// 			name: 'Dribbble',
+		// 			link: 'https://www.dribbble.com',
+		// 		},
+		// 		{
+		// 			name: 'Trello',
+		// 			link: 'https://www.trello.com',
+		// 		},
+		// 		{
+		// 			name: 'Slack',
+		// 			link: 'https://www.slack.com',
+		// 		},
+		// 	],
+		// },
 	],
 
 	// Second Links Container
 	secondListsContainer: [
-		{
-			icon: 'binary',
-			id: '1',
-			links: [
-				{
-					name: 'Spotify',
-					link: 'https://www.spotify.com',
-				},
-				{
-					name: 'Reddit',
-					link: 'https://www.reddit.com',
-				},
-				{
-					name: 'Hashnode',
-					link: 'https://www.hashnode.com',
-				},
-				{
-					name: 'Pocket',
-					link: 'https://www.pocket.com',
-				},
-			],
-		},
+		// {
+		// 	icon: 'binary',
+		// 	id: '1',
+		// 	links: [
+		// 		{
+		// 			name: 'Spotify',
+		// 			link: 'https://www.spotify.com',
+		// 		},
+		// 		{
+		// 			name: 'Reddit',
+		// 			link: 'https://www.reddit.com',
+		// 		},
+		// 		{
+		// 			name: 'Hashnode',
+		// 			link: 'https://www.hashnode.com',
+		// 		},
+		// 		{
+		// 			name: 'Pocket',
+		// 			link: 'https://www.pocket.com',
+		// 		},
+		// 	],
+		// },
 		{
 			icon: 'github',
 			id: '2',
 			links: [
-				{
-					name: 'Front',
-					link: 'https://www.reddit.com/r/Frontend/',
-				},
+				// {
+				// 	name: 'Front',
+				// 	link: 'https://www.reddit.com/r/Frontend/',
+				// },
 				{
 					name: 'Rust',
 					link: 'https://www.reddit.com/r/rust/',
@@ -221,8 +215,8 @@ const CONFIG = {
 					link: 'https://www.reddit.com/r/golang/',
 				},
 				{
-					name: 'Repos',
-					link: 'https://github.com/migueravila',
+					name: 'This week in rust',
+					link: 'https://this-week-in-rust.org/',
 				},
 			],
 		},
